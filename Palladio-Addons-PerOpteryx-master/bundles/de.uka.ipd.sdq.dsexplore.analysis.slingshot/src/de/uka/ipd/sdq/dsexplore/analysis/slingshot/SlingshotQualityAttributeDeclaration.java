af25823b3141c076792f8a15202ef141b5e1a567
alpetkova <sandita97@gmail.com>
2025-03-23 22:43:47 +0100

    Slingshot-PerOpteryx Interface Files

---
+++ Palladio-Addons-PerOpteryx-master/bundles/de.uka.ipd.sdq.dsexplore.analysis.slingshot/src/de/uka/ipd/sdq/dsexplore/analysis/slingshot/SlingshotQualityAttributeDeclaration.java
@@ -0,0 +1,39 @@
+package de.uka.ipd.sdq.dsexplore.analysis.slingshot;
+
+import org.eclipse.emf.ecore.util.EcoreUtil;
+
+import de.uka.ipd.sdq.dsexplore.analysis.AbstractPerformanceAttributeDeclaration;
+import de.uka.ipd.sdq.dsexplore.analysis.IAnalysisQualityAttributeDeclaration;
+import de.uka.ipd.sdq.dsexplore.qml.contract.QMLContract.EvaluationAspect;
+import de.uka.ipd.sdq.dsexplore.qml.contract.QMLContract.Mean;
+import de.uka.ipd.sdq.dsexplore.qml.contracttype.QMLContractType.Dimension;
+
+/**
+ * Defines the Dimensions of this analyzer.
+ *
+ * @author Dominik Fuchss
+ *
+ */
+public class SlingshotQualityAttributeDeclaration extends AbstractPerformanceAttributeDeclaration implements IAnalysisQualityAttributeDeclaration {
+
+	public SlingshotQualityAttributeDeclaration() {
+		super();
+
+	}
+
+	@Override
+	public boolean canEvaluateAspectForDimension(EvaluationAspect aspect, Dimension dimension) {
+		if (EcoreUtil.equals(dimension, this.responseTimeDimension) && aspect instanceof Mean) {
+			return true;
+		}
+		// if (EcoreUtil.equals(dimension, this.throughputDimension) && aspect
+		// instanceof Mean) {
+		// return true;
+		// }
+		// if (EcoreUtil.equals(dimension, this.maxUtilizationDimension)) {
+		// return true;
+		// }
+		return false;
+	}
+
+}
